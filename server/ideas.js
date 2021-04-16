93ab5007ad930782a04c8d30b3244d4647718640
Elias Zoghaib <elias.zoghaib@dal.ca>
2021-04-16 12:09:54 -0300

    Refactored idea put command

--- server/ideas.js
+++ server/ideas.js
@@ -32,22 +32,8 @@ ideaRouter.get("/:ideaId", (req, res) => {
 });
 
 ideaRouter.put("/:ideaId", (req, res) => {
-    const retreivedIdea = getFromDatabaseById("ideas", req.params.ideaId);
-    const { name, description, numWeeks, weeklyRevenue } = req.body;
-    if (retreivedIdea && name && description && numWeeks && weeklyRevenue) {
-        retreivedIdea.name = name;
-        retreivedIdea.description = description;
-        retreivedIdea.numWeeks = Number(numWeeks);
-        retreivedIdea.weeklyRevenue = Number(weeklyRevenue);
-        const updatedIdea = updateInstanceInDatabase("ideas", retreivedIdea);
-        if (updatedIdea) {
-            res.status(200).send(updatedIdea);
-        } else {
-            res.status(404).send();
-        }
-    } else {
-        res.status(404).send();
-    }
+    let updatedInstance = updateInstanceInDatabase("ideas", req.body);
+    res.status(200).send(updatedInstance);
 });
 
 ideaRouter.delete("/:ideaId", (req, res) => {

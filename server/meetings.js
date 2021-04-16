23ae48435bc46e9804bb30352897e190b9dd5181
Elias Zoghaib <elias.zoghaib@dal.ca>
2021-04-15 23:27:02 -0300

    Meeting api routes

--- server/meetings.js
+++ server/meetings.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const meetingRouter = express.Router();
-
+const {
+    getAllFromDatabase,
+    addToDatabase,
+    deleteAllFromDatabase,
+    createMeeting,
+} = require("./db");
 module.exports = meetingRouter;
+
+meetingRouter.get("/", (req, res) => {
+    res.status(200).send(getAllFromDatabase("meetings"));
+});
+
+meetingRouter.post("/", (req, res) => {
+    const newMeeting = createMeeting();
+    addToDatabase("meetings", newMeeting);
+    res.status(201).send(newMeeting);
+});
+meetingRouter.delete("/", (req, res) => {
+    deleteAllFromDatabase("meetings");
+    res.status(204).send();
+});
